dc9dc75d816a28f2b45a822d0c6a41fa419eb5e4
unknown <matheus_santana@hotmail.com.br>
2021-08-05 16:31:38 -0300

    Atualização CSS Tempo

--- tempo.php
+++ tempo.php
@@ -9,24 +9,27 @@ $tempo = $_SESSION['tempo'];
 
 <style>
   .body {
-    background-image: url('https://i2.wp.com/wallpaperaccess.com/full/1250040.jpg ');
+    background-color: #70bfff80;
     background-position: center;
     background-size: cover;
     align-items: center;
     justify-items: center;
-    background-color: lavender;
 
   }
 
   .nome {
-    font-size: 52px;
-    margin-left: 38%;
-    margin-top: 10px;
+    font-size: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   .lonlat {
-    margin-top: 30%;
+    margin-top: 10%;
     font-family: monospace;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   .voltar {
@@ -40,11 +43,44 @@ $tempo = $_SESSION['tempo'];
     background-color: silver;
     color: black;
     border: 2px solid silver;
-    
+
   }
 
-  .vtr{
-    margin: 7px 5px;
+  .vtr {
+    margin: 22px 14px;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .temperatura {
+    margin-bottom: 20px;
+    margin-top: 20px;
+    font-size: 35px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .estado {
+    font-size: 32;
+    font-family: system-ui;
+    margin-right: 100px;
+  }
+
+  .velumi{
+    font-size: 32;
+    font-family: system-ui;
+    margin-right: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
   }
 </style>
 
@@ -55,7 +91,7 @@ $tempo = $_SESSION['tempo'];
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Clima e Tempo</title>
 </head>
 
 
@@ -67,30 +103,32 @@ $tempo = $_SESSION['tempo'];
   <div class="nome">
     <?php echo $tempo->name; ?>
   </div>
+  <div class="temperatura"><?php echo number_format((($tempo->main->temp / 5) - 32) * 5/9) . "°" ?> </div>
+
 
   <div>
-    <div><?php echo $tempo->weather[0]->icon; ?></div>
-    <div><?php echo $tempo->weather[0]->description; ?></div>
-  </div>
 
-  <div><?php echo "Velocidade do vento: " . ($tempo->wind->speed) . "KM/h" ?></div>
-
-  <div>
-    <div class="max"><?php echo " TEMP: " . number_format((($tempo->main->temp_max / 5) - 32) * 5 / 9) . "°" ?> </div>
-    <div class="umidade"><?php echo "UMIDADE: " . $tempo->main->humidity ?> </div>
-  </div>
+    <div class="icon">
+      <div class="estado"><?php echo ucfirst($tempo->weather[0]->description); ?></div>
+      <img src="http://openweathermap.org/img/wn/<?php echo $tempo->weather[0]->icon ?>@4x.png">
+    </div>
 
 
-
-  <div class="lonlat">
-    <?php echo "Longitude: " . $tempo->coord->lon; ?>
-    <?php echo "Latitude: " . $tempo->coord->lat; ?>
-  </div>
+    <div class="velumi">
+    <div><?php echo "Vento: " . $tempo->wind->speed . "KM/h" ?></div>
+    <div>
+      <div class="umidade"><?php echo "UMIDADE: " . $tempo->main->humidity . "%" ?> </div>
+    </div>
+    </div>
+    <div class="lonlat">
+      <?php echo "Longitude: " . $tempo->coord->lon; ?>
+      <?php echo "Latitude: " . $tempo->coord->lat; ?>
+    </div>
   </div>
 </body>
 
 <footer>
- <div class="vtr"> <a href="index.php" class="voltar">Voltar</a></div>
+  <div class="vtr"> <a href="index.php" class="voltar">Nova Consulta</a></div>
 </footer>
 
 </html>
